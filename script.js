2e6ba6e4aaf89a586371ce8dd204bf215c3bb909
Gabe Lyu <no6xichengavenue@gmail.com>
2019-04-05 09:46:53 +0100

    Super text added

--- script.js
+++ script.js
@@ -1,19 +1,54 @@
-const textBlock = document.getElementById("text");
+const SUPER_START_SIGNAL = /^\$\$/;
+const SUPER_END_SIGNAL = /.+\$\$/;
+const SUPER_MARK = /\$\$/;
+
+const textBlock = document.getElementById("textBlock");
 
 let words = text.split(/ /g);
+let target = textBlock;
+
+function superBegin() {
+  let textSpan = document.createElement("span");
+  textSpan.classList.add("super");
+  textBlock.append(textSpan);
+  target = textSpan;
+}
+
+function superEnd() {
+  let textSpan = document.createElement("p");
+  textSpan.classList.add("normal");
+  textBlock.append(textSpan);
+  target = textSpan;
+}
 
 function render() {
-  if (words.length === 0) window.clearInterval(a);
-  else {
-    let blank = textBlock.textContent === "";
-    if (blank) textBlock.textContent += `${words[0]}`;
-    else textBlock.textContent += ` ${words[0]}`;
-    words.shift();
+  let begin = SUPER_START_SIGNAL.test(words[0]);
+  let end = SUPER_END_SIGNAL.test(words[0]);
+  let word = words[0].split(SUPER_MARK).join("");
+
+  if (begin) superBegin();
+
+  let blank = target.textContent === "";
+  if (blank) {
+    target.textContent += `${word}`;
+  } else {
+    target.textContent += ` ${word}`;
   }
+
+  if (end) superEnd();
+
+  words.shift();
+  console.log(begin, end, textBlock.innerHTML);
+
   if (textBlock.scrollHeight > textBlock.clientHeight) {
     textBlock.style.marginTop = `calc(540px - ${textBlock.scrollHeight}px)`;
-    console.log(textBlock.scrollHeight, textBlock.clientHeight);
+  }
+
+  if (words.length === 0) {
+    window.clearInterval(handle);
   }
 }
 
-let a = window.setInterval(render, 500);
+superEnd();
+
+let handle = window.setInterval(render, 500);
